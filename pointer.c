566279238238203b64b3c54da2d3eb07668cc6ef
kailashnandan288-hub <kailashnandan288@gmail.com>
2026-07-29 12:45:06 +0530

    “afsfa”

---
+++ pointer.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    int flat=23;
+    char a='F';
+    char b[]='Kailash';
+    float y=34.2
+    int *building=&flat;
+    int arr[]={23,464,352,14,53,2234,2342,234,7686,857};
+
+ printf("The address of array is %d\n",&arr);
+    printf("The address of 0th value in array is %d",&arr[1]);
+}
